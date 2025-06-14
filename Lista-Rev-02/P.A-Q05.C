75b3612d73da357c0ee9bbc1770fb0f383e43729
taygo <taygocezar@gmail.com>
2025-06-14 10:01:40 -0300

    Fibonacci questions 2 and 5

--- Lista-Rev-02/P.A-Q05.C
+++ Lista-Rev-02/P.A-Q05.C
@@ -1,19 +1,21 @@
 #include <stdio.h>
-//Questão 2
+//Questão 5
 int main() 
 {
-    int i = 0, j = 1, n, aux;
+    int i = 0, j = 1, n, aux, sum = 0;
     
     printf("insert a number: "); 
     scanf("%d",&n);
     
     for(int k = 0; k < n; k++)
     {
-        printf("%d\n", i);
+        sum += i;
         aux = i + j;
         i = j;
         j = aux;
     }
     
+    printf("the total sum is equal to: %d", sum);
+
     return 0;
 }
